5f931250f271e1b1192ed98c60517d70212eb6d8
Melissa Moyer <melissamoyer@Melissas-MacBook-Pro.local>
2025-01-06 17:23:54 -0600

    commiting rest of changes from att 1

--- index.js
+++ index.js
@@ -5,5 +5,30 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {
+  FingerprintJsProProvider
+} from '@fingerprintjs/fingerprintjs-pro-react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+const WrappedApp = () => (
+  <FingerprintJsProProvider
+    apiKey={'X876qE3W4DAYxlqO7sE4'}
+//    endpointUrl={'https://metrics.melissamoyerp.com'}
+  >
+    <App />
+  </FingerprintJsProProvider>
+);
+
+AppRegistry.registerComponent('AppName', () => WrappedApp);
+
+
+//AppRegistry.registerComponent(
+//  'AppName',
+//  <FingerprintJsProProvider
+//      apiKey={'X876qE3W4DAYxlqO7sE4'}
+//  >
+//    <App />
+//  </FingerprintJsProProvider>
+//);e
+
+
+//AppRegistry.registerComponent(appName, () => App);
